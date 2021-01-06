826ceec4fcf8a991d6f3eac4bc6f0d209510a6ea
Tawnee Harris <tawneeh@icloud.com>
2021-01-06 11:58:52 -0800

    add Character Controllers
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>
    Co-authored-by: Patrick Osten <posten.coding@gmail.com>

---
+++ BOTW/Controllers/CharactersController.cs
@@ -0,0 +1,116 @@
+using BOTW.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BOTW.Controllers
+{
+  public class CharactersController : Controller
+  {
+    private readonly BOTWContext _db;
+    public CharactersController(BOTWContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Character> model = _db.Characters.ToList();
+      return View(_db.Characters.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Character Character)
+    {
+      _db.Characters.Add(Character);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisCharacter = _db.Characters //this is called a stream. streams let you enumerate over all of the items in a collection. thisCharacter is a set until other things are called on it. but since you're calling these other things it is just the culmination of all of them at once.
+        .Include(character => character.Locations) // (character => character.JoinEntries) this is a lambda function, a nameless method, to the left of the equals is what the method takes, to the right of the equals is what the method does. item can be whatever (x, a, y.)
+        // include function iterates over the dbCharacters set, character is an individual character in the set, this will call categories on every item in the database and load them into the individual item in the lambda object. 
+        // this returns a collection of join entities, which is the collection of relationships between the item and the categores, as defined by the categoryItems table in the database 
+        // a collection is an object that contains other typed objects, and offers some specific methods on how to access them. 
+        // in this case, the join entities are CharacterLocationStyle Objects.
+        .ThenInclude(join => character.JoinEntries) //join should really be CharacterLocationStyle.
+        .Include(character => character.Styles)
+        .ThenInclude(join => join.JoinEntries)
+        .FirstOrDefault(character => character.CharacterId == id); //specifies which particular character to run these commands on / filter list by
+      return View(thisCharacter);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisCharacter = _db.Characters.FirstOrDefault(character => character.CharacterId == id);
+      return View(thisCharacter);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Character Character)
+    {
+      _db.Entry(Character).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddStyle(int id)
+    {
+      var thisCharacter = _db.Characters.FirstOrDefault(CharactersController => CharactersController.CharacterId == id);
+      ViewBag.StyleId = new SelectList(_db.Styles, "StyleId", "Type");
+      return View(thisCharacter);
+    }
+
+    [HttpPost]
+    public ActionResult AddStyle(Character character, int StyleId)
+    {
+      if (StyleId != 0)
+      {
+        _db.CharacterLocationStyle.Add(new CharacterLocationStyle() { StyleId = StyleId, CharacterId = character.CharacterId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult AddLocation(int id)
+    {
+      var thisCharacter = _db.Characters.FirstOrDefault(CharactersController => CharactersController.CharacterId == id);
+      ViewBag.LocationId = new SelectList(_db.Locations, "LocationId", "Name");
+      return View(thisCharacter);
+    }
+
+    [HttpPost]
+    public ActionResult AddLocation(Character character, int LocationId)
+    {
+      if (LocationId != 0)
+      {
+        _db.CharacterLocationStyle.Add(new CharacterLocationStyle() { LocationId = LocationId, CharacterId = character.CharacterId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisCharacter = _db.Characters.FirstOrDefault(character => character.CharacterId == id);
+      return View(thisCharacter);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisCharacter = _db.Characters.FirstOrDefault(character => character.CharacterId == id);
+      _db.Characters.Remove(thisCharacter);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
