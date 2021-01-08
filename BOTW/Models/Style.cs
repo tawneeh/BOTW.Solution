d343365a1e4ff39b0d4d03584b40ca0b1d38c9d1
Tawnee Harris <tawneeh@icloud.com>
2021-01-07 17:24:52 -0800

    add notes
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>
    Co-authored-by: Patrick Osten <posten.coding@gmail.com>

--- BOTW/Models/Style.cs
+++ BOTW/Models/Style.cs
@@ -6,11 +6,13 @@ namespace BOTW.Models
   {
     public Style()
     {
-      this.JoinEntries = new HashSet<CharacterLocationStyle>();
+      this.JoinEntries = new HashSet<CharacterLocationStyle>(); // part of the constructor
     }
 
-    public int StyleId { get; set; }
+    public int StyleId { get; set; } // definitions of properties
     public string Name { get; set; }
+    public bool Owned { get; set; } 
     public virtual ICollection<CharacterLocationStyle> JoinEntries { get; set; }
+    static public ICollection<Style> StyleList { get; set; }
   }
 }
